21d53355782c1cd3e350fe9458daffd692559994
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-07-04 12:50:50 +0200

    sha1

---
+++ file.sh
@@ -0,0 +1,88 @@
+$ find -type f -exec sha1sum '{}' \; | grep -v git
+9b38a63f08d8cbc834067eac2b42dcb30413ed6f *./cours1.md
+16a93087e3978f3de4712df7f91cf079e90911d5 *./cours2/.vscode/settings.json
+ee45961456143004c789cd6d5485017146e54cb1 *./cours2/cours2.md
+466016a4dad6a64d6af517fd3e99938221e4cfb1 *./cours2/downloads/mail1_from_rasslan.md
+9a2df3d1c2a1e2ad46c6aa6393a5c620e0badb69 *./cours2/downloads/wikihow.pdf
+04bccadf902e980991bd03024beb2e03a926f44a *./cours2/mydocs/cours2-td2_AMINE.docx
+9c29ddcec1ac0e2131daeee8cfd040d4aaf39d4e *./cours2/mydocs/cours2-td2_AMINE.pdf
+286d820701086dde564038ecd0f18f38516f1a28 *./cours2/mydocs/mail/mail1.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours2/mydocs/mail/mail2.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours2/mydocs/mail/mail3.md
+53e3ffbd05e7bcc3736907e25dce8b1d216a2e0d *./cours2/mydocs/mytd2.md
+0d65a039feccb930cc323f7fd4a2f35ed5b47d0e *./cours3/cours3.md
+0a772c9a45de75a3b07ab7191fd3a28e3ab9b8fd *./cours3/downloads/mail1_from_rasslan.md
+05c0356801602faee82557d7be1d65efee73457b *./cours3/downloads/mail3_from_rasslan.md
+98dd7e208894b74d7983abf0ad71bc5dba718991 *./cours3/mydocs/cours3-td3_AMINE.docx
+e5dfb8da2525a29fc12bccebc4971ada5186d455 *./cours3/mydocs/mail/mail1.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours3/mydocs/mail/mail2.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours3/mydocs/mail/mail3.md
+c5b8a9e1e1b117110ebe2a9e872c912131b4fe0f *./cours3/mydocs/mytd3.md
+d27af0a03baf2f2d7fa9be4f84043c2650edd672 *./cours4/cours4.md
+544b3745c213c924c77fb42035e10f41196d7a41 *./cours4/downloads/mail1_from_rasslan.md
+602fc47c23fdc4c87b1416d654f2c004fe22ce41 *./cours4/mydocs/cours4-td4_AMINE.docx
+f636b9405ccec9f0b02afccc637f25818ab4326c *./cours4/mydocs/mail/mail1.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours4/mydocs/mail/mail2.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours4/mydocs/mail/mail3.md
+3811ac1ebc2ccb8b6076f9ae56b3c94764caf81a *./cours4/mydocs/mytd4.md
+3c0ca49332cdcc8b5bd09a144b9d67fbc2a6e1f0 *./cours5/cours5-td5_AMINE.docx
+3affb3f454b18e02264d3d0a2b7eb83173de4b78 *./cours5/cours5.md
+a6252ce4c860142580f7206a5d5be2ca3ac26705 *./cours5/TD5 du 09 JUIN - Comment réussir l'animation d'une réunion.pdf
+1daaba8f0a31d326754e685ffb847c3f68de93e9 *./cours6/cours6-td6_AMINE (1)_v5.docx
+56248204845decebc8c8b335c1a15b28a34796af *./downloads/Connaître les bases de la communication professionnelle.pdf
+3180b7eb8387fc8c481d8ba303d18eca0e3e0d14 *./downloads/mail1_from_rasslan.md
+ba703cfbb620b9b9a9d61486048263a113108bc1 *./mydocs/cours1-td1_AMINE.docx
+2398fcd634c3c2925b9f98b2d2c4a14d74131f73 *./mydocs/cours1-td1_AMINE.pdf
+cd61cc0544cabbbee3200eb48e833a2f8f9be795 *./mydocs/cours1-td1_AMINE_simple.docx
+52bb77876d26bc73b4ce28cd2c3e7687d8a8e424 *./mydocs/cours1-td1_AMINE_v2.pdf
+c62e6957f65a6e6420733a91cc5490fca1fe06f6 *./mydocs/mail/mail1.md
+ede29fb20cd2626c0e4246e8241a8803c9a79495 *./mydocs/mail/mail2.md
+fe46679478fd04dc0c3011c5b0e1f7875e566f7d *./mydocs/mail/mail3.md
+925e98ea783cde05759ad5f4ecea1f7a4c2172ce *./others/extension-vscode.md
+d5248538672adccdac46b8d1b954d2e826681c78 *./README.md
+
+
+$ find -type f -exec sha1sum '{}' \;
+9b38a63f08d8cbc834067eac2b42dcb30413ed6f *./cours1.md
+16a93087e3978f3de4712df7f91cf079e90911d5 *./cours2/.vscode/settings.json
+ee45961456143004c789cd6d5485017146e54cb1 *./cours2/cours2.md
+466016a4dad6a64d6af517fd3e99938221e4cfb1 *./cours2/downloads/mail1_from_rasslan.md
+9a2df3d1c2a1e2ad46c6aa6393a5c620e0badb69 *./cours2/downloads/wikihow.pdf
+04bccadf902e980991bd03024beb2e03a926f44a *./cours2/mydocs/cours2-td2_AMINE.docx
+9c29ddcec1ac0e2131daeee8cfd040d4aaf39d4e *./cours2/mydocs/cours2-td2_AMINE.pdf
+286d820701086dde564038ecd0f18f38516f1a28 *./cours2/mydocs/mail/mail1.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours2/mydocs/mail/mail2.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours2/mydocs/mail/mail3.md
+53e3ffbd05e7bcc3736907e25dce8b1d216a2e0d *./cours2/mydocs/mytd2.md
+16a93087e3978f3de4712df7f91cf079e90911d5 *./cours3/.vscode/settings.json
+0d65a039feccb930cc323f7fd4a2f35ed5b47d0e *./cours3/cours3.md
+0a772c9a45de75a3b07ab7191fd3a28e3ab9b8fd *./cours3/downloads/mail1_from_rasslan.md
+05c0356801602faee82557d7be1d65efee73457b *./cours3/downloads/mail3_from_rasslan.md
+98dd7e208894b74d7983abf0ad71bc5dba718991 *./cours3/mydocs/cours3-td3_AMINE.docx
+e5dfb8da2525a29fc12bccebc4971ada5186d455 *./cours3/mydocs/mail/mail1.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours3/mydocs/mail/mail2.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours3/mydocs/mail/mail3.md
+c5b8a9e1e1b117110ebe2a9e872c912131b4fe0f *./cours3/mydocs/mytd3.md
+16a93087e3978f3de4712df7f91cf079e90911d5 *./cours4/.vscode/settings.json
+d27af0a03baf2f2d7fa9be4f84043c2650edd672 *./cours4/cours4.md
+544b3745c213c924c77fb42035e10f41196d7a41 *./cours4/downloads/mail1_from_rasslan.md
+05c0356801602faee82557d7be1d65efee73457b *./cours4/downloads/mail3_from_rasslan.md
+602fc47c23fdc4c87b1416d654f2c004fe22ce41 *./cours4/mydocs/cours4-td4_AMINE.docx
+f636b9405ccec9f0b02afccc637f25818ab4326c *./cours4/mydocs/mail/mail1.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours4/mydocs/mail/mail2.md
+da39a3ee5e6b4b0d3255bfef95601890afd80709 *./cours4/mydocs/mail/mail3.md
+3811ac1ebc2ccb8b6076f9ae56b3c94764caf81a *./cours4/mydocs/mytd4.md
+56248204845decebc8c8b335c1a15b28a34796af *./downloads/Connaître les bases de la communication professionnelle.pdf
+3180b7eb8387fc8c481d8ba303d18eca0e3e0d14 *./downloads/mail1_from_rasslan.md
+ba703cfbb620b9b9a9d61486048263a113108bc1 *./mydocs/cours1-td1_AMINE.docx
+2398fcd634c3c2925b9f98b2d2c4a14d74131f73 *./mydocs/cours1-td1_AMINE.pdf
+cd61cc0544cabbbee3200eb48e833a2f8f9be795 *./mydocs/cours1-td1_AMINE_simple.docx
+52bb77876d26bc73b4ce28cd2c3e7687d8a8e424 *./mydocs/cours1-td1_AMINE_v2.pdf
+c62e6957f65a6e6420733a91cc5490fca1fe06f6 *./mydocs/mail/mail1.md
+ede29fb20cd2626c0e4246e8241a8803c9a79495 *./mydocs/mail/mail2.md
+fe46679478fd04dc0c3011c5b0e1f7875e566f7d *./mydocs/mail/mail3.md
+925e98ea783cde05759ad5f4ecea1f7a4c2172ce *./others/extension-vscode.md
+d5248538672adccdac46b8d1b954d2e826681c78 *./README.md
+
+
+
